6a4d85304a041a1f15717c8d8f4120c5c87ef288
nullpointer-1 <pointersnull77@gmail.com>
2025-06-23 20:29:56 +0530

    Add files via upload

---
+++ Frontend/vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  define: {
+    // Replace 'global' with 'globalThis' for browser compatibility
+    // 'globalThis' is a standardized way to access the global object
+    // across different JavaScript environments (browser, Node.js, Web Workers).
+    global: 'globalThis',
+  },
+}));
